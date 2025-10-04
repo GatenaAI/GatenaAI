5a3986e04a33767531229a4630363e17d534716a
Fernanda Martínez <fernandamartinez1@upb.edu>
2025-10-03 20:44:56 -0400

    wall of fame included
    wall of fame included

--- src/components/Home/AboutUsSection.js
+++ src/components/Home/AboutUsSection.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useLanguage } from '../../context/LanguageContext';
 import '../../styles/home/AboutUsSection.css';
 
 export default function AboutUsSection({ t }) {
+  const { language } = useLanguage();
+
+  const scrollMarginTop = language === 'de' ? '50px' : '90px';
+
   return (
-    <section id="aboutUs" className="about-section">
+    <section
+      id="aboutUs"
+      className="about-section"
+      style={{ scrollMarginTop }}
+    >
       <div className="about-container">
+        {/* LEFT COLUMN */}
         <div className="about-left fade-in">
           <div className="about-left-inner">
             <h2 className="about-title">{t.aboutUs}</h2>
             <p className="about-description">{t.aboutContentOne}</p>
             <p className="about-description">{t.aboutContentTwo}</p>
             <p className="about-description">{t.aboutContentThree}</p>
+            <button className="wallOfFame-btn">{t.wallOfFame}</button>
           </div>
         </div>
 
+        {/* RIGHT COLUMN */}
         <div className="about-right slide-in">
           <div className="about-block">
             <h3 className="block-title">{t.mission}</h3>
@@ -38,6 +50,7 @@ AboutUsSection.propTypes = {
     aboutContentOne: PropTypes.string.isRequired,
     aboutContentTwo: PropTypes.string.isRequired,
     aboutContentThree: PropTypes.string.isRequired,
+    wallOfFame: PropTypes.string.isRequired,
     mission: PropTypes.string.isRequired,
     mission_goal: PropTypes.string.isRequired,
     mission_social: PropTypes.string.isRequired,
